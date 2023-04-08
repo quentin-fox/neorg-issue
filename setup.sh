dea122bb2f3f7255a2cf8911a8711f5da5417967
Quentin Fox <quentinbennettfox@gmail.com>
2023-04-08 18:09:22 -0400

    tweak script to make easier to recreate

--- setup.sh
+++ setup.sh
@@ -14,3 +14,5 @@ env XDG_CONFIG_HOME=$(pwd) XDG_DATA_HOME="$TEMP_DIR/share" XDG_STATE_HOME="$TEMP
 
 echo "\n"
 echo "to remove the temp directory created for this recreation, run 'sudo rm -r $TEMP_DIR'"
+
+echo "to re-open nvim, run 'env XDG_CONFIG_HOME=$(pwd) XDG_DATA_HOME=$TEMP_DIR/share XDG_STATE_HOME=$TEMP_DIR/state nvim'"
